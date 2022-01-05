8f5e94fc255591e86aa7b9aa98a46341d916df70
gojgho <gojgho@gmail.com>
2022-01-05 12:19:14 +0900

    ignore commit

--- src/main/java/com/gojgho/book/springboot/web/IndexController.java
+++ src/main/java/com/gojgho/book/springboot/web/IndexController.java
@@ -3,9 +3,11 @@ package com.gojgho.book.springboot.web;
 
 import com.gojgho.book.springboot.config.auth.LoginUser;
 import com.gojgho.book.springboot.config.auth.dto.SessionUser;
+import com.gojgho.book.springboot.domain.user.UserRepository;
 import com.gojgho.book.springboot.service.PostsService;
 import com.gojgho.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,12 +15,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.servlet.http.HttpSession;
 
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
 
     private final HttpSession httpSession;
+
+    @Autowired
+    UserRepository userRepository;
 
     @GetMapping("/")
     public String index(Model model, @LoginUser SessionUser user) //1
@@ -28,14 +34,25 @@ public class IndexController {
 
         if(user != null){
             model.addAttribute("userName",user.getName());
+            model.addAttribute("userPicture",user.getPicture());
+            model.addAttribute("userEmail",user.getEmail());
         }
         return "index";
     }
 
+
     @GetMapping("/posts/save")
-    public String postsSave(){
+    public String postsSave(Model model, @LoginUser SessionUser user){
+
+        if(user != null){
+            model.addAttribute("userName",user.getName());
+            model.addAttribute("userPicture",user.getPicture());
+            model.addAttribute("userEmail",user.getEmail());
+        }
+
         return "posts-save";  //posts/save를 호출하면 posts-save.mustache 파일 호출하는 메소드가 추가 됨
     }
+
 
     @GetMapping("/posts/update/{id}")
     public String postsUpdate(@PathVariable Long id, Model model){
